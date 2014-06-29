b81410f17dc4a3d3d6e3cdd1ec25d2bb7bdd178d
tetsuo <ogunduz@gmail.com>
2014-06-29 17:48:16 +0300

    ocd

--- test/fixtures/lambdas/index.js
+++ test/fixtures/lambdas/index.js
@@ -1,7 +1,7 @@
 module.exports = function(ctx) {
   return require('./child.mustache').render(ctx, {
-    parent: '{{<older}}{{$a}}p{{/a}}{{$b}}{{#lambda}}p{{/lambda}}{{/b}}{{/older}}',
-    older: '{{<grandParent}}{{$a}}o{{/a}}{{$c}}{{#lambda}}o{{/lambda}}{{/c}}{{/grandParent}}',
-    grandParent: '{{$a}}g{{/a}} - {{$b}}g{{/b}} - {{$c}}g{{/c}} - {{#lambda}}g{{/lambda}}'
+    parent: require('./parent.mustache'),
+    older: require('./older.mustache'),
+    grandparent: '{{$a}}g{{/a}} - {{$b}}g{{/b}} - {{$c}}g{{/c}} - {{#lambda}}g{{/lambda}}'
   });
 };
